1fcc67749527492f3063425c373b81d71526b01e
riccardomicheletto <riccardo.micheletto@studenti.unitn.it>
2020-05-18 11:45:39 +0200

    Add all necessary for a thread that says that mosquitto is alive every 5 seconds

---
+++ IaaS/cloud-init-mosquitto_subscriber_isAlive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y wget software-properties-common
+apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+apt-get update
+apt-get install -y mosquitto mosquitto-clients
+mosquitto_sub -h 172.17.0.2 -p 30000 -t alive
+
+exit 0
--- IaaS/instance_creation.sh
+++ IaaS/instance_creation.sh
@@ -3,7 +3,7 @@ PROJECTN=mqtt_subscriber
 . /home/stack/devstack/openrc
 . ${PROJECTN}-openrc.sh
 
-# Server creation
+################################################## mosquitto subscriber creation
 openstack server create \
 --flavor micro.ubuntu \
 --image ubuntu-bionic-18.04 \
@@ -21,4 +21,26 @@ openstack server add floating ip subscriber_mqtt "${FLOATING_IP}"
 openstack server add security group subscriber_mqtt ssh-in
 openstack server add security group subscriber_mqtt mqtt-in
 
-echo mqtt_subscriber can be reached at ${FLOATING_IP}
+echo subscriber_mqtt can be reached at ${FLOATING_IP}
+
+
+
+#################################################### isAlive subscriber creation
+openstack server create \
+--flavor micro.ubuntu \
+--image ubuntu-bionic-18.04 \
+--network subscriber_network \
+--key-name eval \
+--security-group default \
+--user-data cloud-init-mosquitto_subscriber_isAlive.sh \
+subscriber_mqtt_isAlive
+
+# Floating IP assignment
+FLOATING_IP2=$(openstack floating ip create public --format json | jq -r -M '.name')
+openstack server add floating ip subscriber_mqtt_isAlive "${FLOATING_IP2}"
+
+# Add security groups
+openstack server add security group subscriber_mqtt_isAlive ssh-in
+openstack server add security group subscriber_mqtt_isAlive mqtt-in
+
+echo subscriber_mqtt_isAlive can be reached at ${FLOATING_IP2}
